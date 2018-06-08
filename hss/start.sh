35400b9ace719a4401843bc13aefe470ad79750d
Balz Aschwanden <balz.aschwanden@unibas.ch>
2018-06-08 16:23:22 +0200

    Only create certificate if not present

--- hss/start.sh
+++ hss/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# MySQL database configuration 
+# MySQL database configuration
 sed -i "s/@MYSQL_user@/$MYSQLUSER/g" /usr/local/etc/oai/hss.conf
 sed -i "s/@MYSQL_pass@/$MYSQLPASSWORD/g" /usr/local/etc/oai/hss.conf
 sed -i "s/127.0.0.1/$MYSQLHOSTNAME/g" /usr/local/etc/oai/hss.conf
@@ -14,18 +14,19 @@ sed -i "s/hss.openair4G.eur/$HSS_CN_NAME/g" /usr/local/etc/oai/hss.conf
 sed -i "s/hss.openair4G.eur/$HSS_CN_NAME/g" /usr/local/etc/oai/freeDiameter/hss_fd.conf
 sed -i "s/hss.openair4G.eur/$HSS_CN_NAME/g" /usr/local/etc/oai/freeDiameter/acl.conf
 
-
-# Generation of certificate for diameter 
+# Generation of certificate for diameter
 cd /root
-mkdir demoCA && touch demoCA/index.txt && echo 01 > demoCA/serial
-openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out hss.cacert.pem -keyout hss.cakey.pem -subj /CN=$HSS_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
-openssl genrsa -out hss.key.pem 1024
-openssl req -new -batch -out hss.csr.pem -key hss.key.pem -subj /CN=$HSS_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
-openssl ca -cert hss.cacert.pem -keyfile hss.cakey.pem -in hss.csr.pem -out hss.cert.pem -outdir . -batch
-mv /root/hss.cakey.pem /usr/local/etc/oai/freeDiameter/
-mv /root/hss.cert.pem /usr/local/etc/oai/freeDiameter/
-mv /root/hss.cacert.pem /usr/local/etc/oai/freeDiameter/
-mv /root/hss.key.pem /usr/local/etc/oai/freeDiameter/
+if [[ ! -d /root/demoCA ]];then
+	mkdir demoCA && touch demoCA/index.txt && echo 01 > demoCA/serial
+	openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out hss.cacert.pem -keyout hss.cakey.pem -subj /CN=$HSS_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+	openssl genrsa -out hss.key.pem 1024
+	openssl req -new -batch -out hss.csr.pem -key hss.key.pem -subj /CN=$HSS_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+	openssl ca -cert hss.cacert.pem -keyfile hss.cakey.pem -in hss.csr.pem -out hss.cert.pem -outdir . -batch
+	mv /root/hss.cakey.pem /usr/local/etc/oai/freeDiameter/
+	mv /root/hss.cert.pem /usr/local/etc/oai/freeDiameter/
+	mv /root/hss.cacert.pem /usr/local/etc/oai/freeDiameter/
+	mv /root/hss.key.pem /usr/local/etc/oai/freeDiameter/
+fi
 
 # Start hss
 sleep 15 && /root/openair-cn/build/hss/build/oai_hss
--- mme/start.sh
+++ mme/start.sh
@@ -10,17 +10,18 @@ sed -i s/"192.168.11.17"/"$MME_IPV4_ADDRESS_FOR_S1_MME"/g /usr/local/etc/oai/mme
 sed -i s/"127.0.0.1"/"$HSS_IPV4_ADDRESS"/g /usr/local/etc/oai/freeDiameter/mme_fd.conf
 
 
-# Generation of certificate for diameter 
+# Generation of certificate for diameter
 cd /root
-mkdir demoCA && touch demoCA/index.txt && echo 01 > demoCA/serial
-openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out mme.cacert.pem -keyout mme.cakey.pem -subj /CN=$MME_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
-openssl genrsa -out mme.key.pem 1024
-openssl req -new -batch -out mme.csr.pem -key mme.key.pem -subj /CN=$MME_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
-openssl ca -cert mme.cacert.pem -keyfile mme.cakey.pem -in mme.csr.pem -out mme.cert.pem -outdir . -batch
-mv /root/mme.cakey.pem /usr/local/etc/oai/freeDiameter/
-mv /root/mme.cert.pem /usr/local/etc/oai/freeDiameter/
-mv /root/mme.cacert.pem /usr/local/etc/oai/freeDiameter/
-mv /root/mme.key.pem /usr/local/etc/oai/freeDiameter/
-
+if [[ ! -d /root/demoCA ]];then
+  mkdir demoCA && touch demoCA/index.txt && echo 01 > demoCA/serial
+  openssl req  -new -batch -x509 -days 3650 -nodes -newkey rsa:1024 -out mme.cacert.pem -keyout mme.cakey.pem -subj /CN=$MME_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+  openssl genrsa -out mme.key.pem 1024
+  openssl req -new -batch -out mme.csr.pem -key mme.key.pem -subj /CN=$MME_CN_NAME/C=FR/ST=PACA/L=Aix/O=Eurecom/OU=CM
+  openssl ca -cert mme.cacert.pem -keyfile mme.cakey.pem -in mme.csr.pem -out mme.cert.pem -outdir . -batch
+  mv /root/mme.cakey.pem /usr/local/etc/oai/freeDiameter/
+  mv /root/mme.cert.pem /usr/local/etc/oai/freeDiameter/
+  mv /root/mme.cacert.pem /usr/local/etc/oai/freeDiameter/
+  mv /root/mme.key.pem /usr/local/etc/oai/freeDiameter/
+fi
 # Start mme
 sleep 17 && /root/openair-cn/build/mme/build/mme
